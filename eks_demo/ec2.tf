f220a2a0cb8fa202dc818c9903267765232a7d85
damowang <308018938@qq.com>
2023-03-15 17:47:22 +0800

    commit

--- eks_demo/ec2.tf
+++ eks_demo/ec2.tf
@@ -6,5 +6,11 @@ resource "aws_launch_template" "eks-template" {
   key_name = local.launch_template_key_name
   vpc_security_group_ids = [aws_security_group.eks-node.id]
   user_data = base64encode("#!/bin/bash\n/etc/eks/bootstrap.sh ${aws_eks_cluster.eks-cluster.name}")
+
+  tag {
+    key   = "Name"
+    value = "eks_node_group"
+    propagate_at_launch = true
+  }
 }
 
